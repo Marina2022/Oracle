809080abdd172b930f8cc9c9e66b7f73795dfbe3
Marina2022 <marusiiiia@yandex.ru>
2025-09-19 17:42:32 +0200

    mobile header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -5,42 +5,85 @@ import ToggleTheme from './ToggleTheme';
 import UserMenu from "@/components/layout/header/UserMenu";
 import Logo from "@/components/shared/Logo";
 import ToggleLang from "@/components/layout/header/ToggleLang";
-import {Menu} from "lucide-react"
+import {Bell, Bot, ChartColumn, Crown, Eye, House, MessageSquare, Settings, Trophy, User, Users} from "lucide-react"
 import {useState} from "react";
+import MobileMenu from "@/components/layout/header/MobileMenu";
 
 const Header = () => {
 
 
-  const [isLogined, setIsLogined] = useState<boolean>(true)
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true)
 
+  const loggedInMenu = [
+    {title: "Главная", link: "/", icon: <House className="h-4 w-4"/>},
+    {title: "Мои прогнозы", link: "/predictions", icon: <Eye className="h-4 w-4"/>},
+    {title: "Турниры", link: "#", icon: <Trophy className="h-4 w-4"/>},
+    {title: "Лиги", link: "#", icon: <Crown className="h-4 w-4"/>},
+    {title: "Профиль", link: "#", icon: <User className="h-4 w-4"/>},
+    {title: "Сообщество", link: "#", icon: <MessageSquare className="h-4 w-4"/>},
+    {title: "Аналитика", link: "#", icon: <ChartColumn className="h-4 w-4"/>},
+    {title: "Битва AI", link: "#", icon: <Bot className="h-4 w-4"/>},
+    {title: "Друзья", link: "#", icon: <Users className="h-4 w-4"/>},
+    {title: "Настройки", link: "#", icon: <Settings className="h-4 w-4"/>},
+  ];
+
+  const loggedOutMenu = [
+    {title: "Главная", link: "/", icon: <House className="h-4 w-4"/>},
+    {title: "Модели ИИ", link: "/#models"},
+    {title: "Как работает", link: "/#working-process"},
+    {title: "Прогнозы", link: "/#predictions"},
+    {title: "Цены", link: "/#prices"},
+  ];
 
   return (
     <header>
       <nav className="fixed top-0 w-full z-50 bg-background/80 backdrop-blur-md border-b border-border glassmorphism">
         <div className="container px-4 py-4 flex items-center justify-between ">
-          <Link href="/">
-            <Logo/>
-          </Link>
-          <div className="hidden lg:flex items-center space-x-10">
-            <Link href="/#models" className="text-foreground hover:text-primary transition-colors">
-              Модели ИИ
-            </Link>
-            <Link href="/#working-process" className="text-foreground hover:text-primary transition-colors">
-              Как работает
-            </Link>
-            <Link href="/#predictions" className="text-foreground hover:text-primary transition-colors">
-              Прогнозы
-            </Link>
-            <Link href="/#prices" className="text-foreground hover:text-primary transition-colors">
-              Цены
+          <div className="flex gap-4 items-center">
+            <MobileMenu loggedInMenu={loggedInMenu} loggedOutMenu={loggedOutMenu} isLoggedIn={isLoggedIn} />
+            <Link href="/" className="md:mr-8">
+              <Logo/>
             </Link>
           </div>
-          <div className="flex gap-4 items-center">
+          {
+            !isLoggedIn && <div className="hidden 2xl:flex items-center space-x-10">
+              {
+                loggedOutMenu.map((item, i) => <Link
+                  key={i}
+                  href={item.link}
+                  className="text-foreground whitespace-nowrap hover:text-primary transition-colors">{item.title}</Link>)
+              }
+            </div>
+          }
+
+          {
+            isLoggedIn && <div className="hidden 2xl:flex items-center space-x-10 text-sm">
+              {
+                loggedInMenu.map((item, i) => <Link
+                  key={i}
+                  href={item.link}
+                  className="text-foreground whitespace-nowrap hover:text-primary transition-colors">{item.title}</Link>)
+              }
+            </div>
+          }
+          <div className="flex gap-2 md:ml-8 items-center">
+
             <ToggleTheme/>
             <ToggleLang/>
-            <UserMenu isLogined={isLogined} setIsLogined={setIsLogined} />
 
-            <Menu className="inline lg:hidden "/>
+            <div className="flex gap-2 items-center">
+              {
+                isLoggedIn && (
+                  <div className="relative mr-2">
+                    <div
+                      className="-top-1 -right-1 absolute rounded-full bg-primary h-3 w-3 text-primary-foreground  z-10 text-[8px] flex justify-center items-center">2
+                    </div>
+                    <Bell className="h-4 w-4 text-foreground "/>
+                  </div>
+                )
+              }
+              <UserMenu isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+            </div>
           </div>
         </div>
       </nav>
